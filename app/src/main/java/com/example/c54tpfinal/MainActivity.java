204caa2c482c4bc8251c5cf135a803f7fcf5eefd
Mailloux Mathieu <e.mmailloux2@etu.cvm.qc.ca>
2022-11-30 15:07:54 -0500

    singleton done

--- app/src/main/java/com/example/c54tpfinal/MainActivity.java
+++ app/src/main/java/com/example/c54tpfinal/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.c54tpfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView testInfo;
+    String url;
+    SingletonVolley sg;
+    JsonObjectRequest jsonRequest;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testInfo = findViewById(R.id.testInfo);
+        sg = SingletonVolley.getInstance(MainActivity.this);
+        SharedPreferences shPref = getApplicationContext().getSharedPreferences("SPOTIFY", 0);
+
+        url = "https://open.spotify.com/artist/3inCNiUr4R6XQ3W43s9Aqi?si=ZysWldN2S2WtSqn00pBP6g";
+
+        jsonRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray array = response.getJSONArray("artist");
+                            testInfo.setText(array.getString(0));
+                        } catch (JSONException jsonException)
+                        {
+                            jsonException.printStackTrace();
+                        }
+                    }
+                }, null)
+                {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<>();
+                        Context sharedPreferences;
+                        String token = shPref.getString("token", "");
+                        String auth = "Bearer " + token;
+                        headers.put("Authorization", auth);
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        return headers;
+                    }
+                };
+
+
+        sg.addToRequestQueue(jsonRequest);
     }
 }
--- app/src/main/java/com/example/c54tpfinal/SingletonVolley.java
+++ app/src/main/java/com/example/c54tpfinal/SingletonVolley.java
@@ -9,13 +9,13 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
-public class RequestQueueSingleton {
-    private static RequestQueueSingleton instance;
+public class SingletonVolley {
+    private static SingletonVolley instance;
     private RequestQueue requestQueue;
     private ImageLoader imageLoader;
     private static Context ctx;
 
-    private RequestQueueSingleton(Context context) {
+    private SingletonVolley(Context context) {
         ctx = context;
         requestQueue = getRequestQueue();
 
@@ -36,9 +36,9 @@ public class RequestQueueSingleton {
                 });
     }
 
-    public static synchronized RequestQueueSingleton getInstance(Context context) {
+    public static synchronized SingletonVolley getInstance(Context context) {
         if (instance == null) {
-            instance = new RequestQueueSingleton(context);
+            instance = new SingletonVolley(context);
         }
         return instance;
     }
